e37d0063ec46ca1f70e2d7bf6082084308e5a799
jaktenstid <jaktenstid1@gmail.com>
2017-05-13 23:10:27 +0300

    Date

--- adamscountyscraper/checkdate.py
+++ adamscountyscraper/checkdate.py
@@ -6,7 +6,7 @@ client = MongoClient(os.environ['MONGODB_URI'])
 
 db = client['adcogov']
 col = db['adcogovrecords']
-dates = [datetime.strptime(d['recordDate'].split(' ')[0].strip(), '%m/%d/%Y %I:%M:%S %p') for d in col.find({},{'recordDate' : 1})]
+dates = [datetime.strptime(d['recordDate'].split(' ')[0].strip(), '%m/%d/%Y') for d in col.find({},{'recordDate' : 1})]
     
 #instr = set([d['instrument'] for d in col.find({},{'instrument' : 1})])
 #print(len(list(instr)))
